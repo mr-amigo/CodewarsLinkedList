b3e3e2296d2e6ca508766d2da271ff3c44adc4a0
mr-amigo <mr.amigo.me@gmail.com>
2026-03-27 17:27:04 +0200

    done linked_lists_move_node

---
+++ simple/linked_lists_move_node/solution.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    head = source
+    source = source.next
+    head.next = dest
+    return Context(source, head)
