1964932afa49ccd68b4dcaf2e8bf856928dbb03d
mr-amigo <mr.amigo.me@gmail.com>
2026-03-27 16:57:11 +0200

    done linked_lists_push_build_one_two_three

---
+++ simple/linked_lists_push_build_one_two_three/solution.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def push(head, data):
+    new_head = Node(data)
+    new_head.next = head
+    return new_head
+
+def build_one_two_three():
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
